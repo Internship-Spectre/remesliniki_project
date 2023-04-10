8819c1cb2b465706b6243c8bd2a3712a01f5b560
Hex <thehexreader@gmail.com>
2023-04-10 14:54:56 +0300

    fixed models, fixed for admin

--- app/main/models.py
+++ app/main/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 
-# Create your models here.
-
-
-class Material(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    quantity = models.IntegerField()
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from main.models import Material
 
 
 def index(request):
     return render(request, 'home/index.html')
-
-
-
-def material(request):
-    materials = Material.objects.all()
-    return render(request, 'home/material.html', {'materials': materials})
