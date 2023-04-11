b4c3881ef79def5aebdba2f7d00d42ec47a3d29f
Hex <thehexreader@gmail.com>
2023-04-11 11:06:57 +0300

    beautify admin, start of profile model

--- app/materials/admin.py
+++ app/materials/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Material
 
-
-admin.site.register(Material)
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Material._meta.get_fields()]
