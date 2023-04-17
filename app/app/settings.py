0a58aab866b13d8741294412b835c2c9eefe5d2c
Sizertt <93645062+Sizertt@users.noreply.github.com>
2023-04-17 20:57:50 +0300

    Make products

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'materials',
     'main',
+    'orders',
+    'products',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- app/products/models.py
+++ app/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from materials.models import Material
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='files/images/products', null=True, blank=True)
+
+    description = models.TextField()
+    quantity = models.IntegerField()
+
+    materials = models.ManyToManyField(Material, blank=True)
+
+    
